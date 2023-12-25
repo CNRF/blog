a976a5f09af1ebc070bfe882c0483e740642bb0d
mark wang <834031172@qq.com>
2023-12-25 17:35:43 +0800

    fix: 新增icon配置

--- docs/.vitepress/configs/head.ts
+++ docs/.vitepress/configs/head.ts
@@ -10,6 +10,7 @@ export const head: HeadConfig[] = [
   ['meta', { name: 'msapplication-TileImage', content: '/favicon.ico' }],
   ['meta', { name: 'baidu-site-verification', content: 'codeva-Whjnr38WFE' }],
   ['link', { rel: 'apple-touch-icon', href: '/favicon.ico' }],
+  ['link', { rel: 'icon', href: '/favicon.ico' }],
   ['link', { rel: 'mask-icon', href: '/favicon.ico', color: '#3eaf7c' }],
   ['link', { rel: 'manifest', href: '/manifest.webmanifest' }]
 ]
