21f72db5f2f76486a592863cba43c0f149dae6e0
mark wang <834031172@qq.com>
2024-08-15 14:37:36 +0800

    docs: 调整导航栏内容

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -1,70 +1,86 @@
-import type { DefaultTheme } from 'vitepress'
+import type {DefaultTheme} from 'vitepress'
 
 // @ts-ignore
 export const nav: DefaultTheme.Config['nav'] = [
-  {
-    text: '编程语言',
-    items: [
-        {
-            text: 'java',
-            items: [
-                {
-                    text: 'java基础',
-                    link: '/java/01.java基础/01.基础知识',
-                    activeMatch: '^/java/01.java基础',
-                },
-                {
-                    text: '多线程和高并发',
-                    link: '/java/02.多线程和高并发/01.理论基础',
-                    activeMatch: '^/linux/05.spring',
-                },
-                {
-                    text: 'JDK集合源码',
-                    link: '/java/03.JDK集合源码/01.Collection类关系图',
-                    activeMatch: '^/linux/03.JDK集合源码',
-                },
-                {
-                    text: 'JVM',
-                    link: '/java/04.JVM/01.JVM相关知识体系详解.md',
-                    activeMatch: '^/linux/04.JVM',
-                },
-                {
-                    text: 'spring',
-                    link: '/java/05.spring/01.源码编译',
-                    activeMatch: '^/linux/05.spring',
-                },
-            ],
-            activeMatch: '^/java',
-        },
-        {
-            text:'前端',
-            items: [
-                {
-                    text: 'HTML',
-                    link: '/web/01.html/01.HTML标签-排序标签',
-                    activeMatch: '^/web/01.html',
-                },
-                {
-                    text: 'CSS',
-                    link: '/web/02.css/01.CSS理论知识.md',
-                    activeMatch: '^/web/02.css',
-                },
-                {
-                    text: 'JavaScript',
-                    link: '/web/03.javascript/01.js基本语法.md',
-                    activeMatch: '^/web/03.javascript',
-                },
-                {
-                    text: 'ES6',
-                    link: '/web/04.ES6/01.let和const.md',
-                    activeMatch: '^/web/04.ES6',
-                }
-            ],
-            activeMatch: "^/web"
-        }
-    ],
-  },
-
+    {
+        text: '后端',
+        items: [
+            {
+                text: 'java',
+                items: [
+                    {
+                        text: 'java基础',
+                        link: '/java/01.java基础/01.基础知识',
+                        activeMatch: '^/java/01.java基础',
+                    },
+                    {
+                        text: '多线程和高并发',
+                        link: '/java/02.多线程和高并发/01.理论基础',
+                        activeMatch: '^/linux/05.spring',
+                    },
+                    {
+                        text: 'JDK集合源码',
+                        link: '/java/03.JDK集合源码/01.Collection类关系图',
+                        activeMatch: '^/linux/03.JDK集合源码',
+                    },
+                    {
+                        text: 'JVM',
+                        link: '/java/04.JVM/01.JVM相关知识体系详解.md',
+                        activeMatch: '^/linux/04.JVM',
+                    },
+                    {
+                        text: 'spring',
+                        link: '/java/05.spring/01.源码编译',
+                        activeMatch: '^/linux/05.spring',
+                    },
+                ],
+                activeMatch: '^/java',
+            },
+        ],
+    },
+    {
+        text: '前端',
+        items: [
+            {
+                text: '基础',
+                items: [
+                    {
+                        text: 'HTML',
+                        link: '/web/01.html/01.HTML标签-排序标签',
+                        activeMatch: '^/web/01.html',
+                    },
+                    {
+                        text: 'CSS',
+                        link: '/web/02.css/01.CSS理论知识.md',
+                        activeMatch: '^/web/02.css',
+                    },
+                    {
+                        text: 'JavaScript',
+                        link: '/web/03.javascript/01.js基本语法.md',
+                        activeMatch: '^/web/03.javascript',
+                    },
+                    {
+                        text: 'ES6',
+                        link: '/web/04.ES6/01.let和const.md',
+                        activeMatch: '^/web/04.ES6',
+                    }
+                ],
+                activeMatch: "^/web"
+            },
+            {
+                text: 'vue',
+                items: [
+                    {
+                        text: '基础',
+                        link: '/vue/01.基础/MVVM模式.md',
+                        activeMatch: '^/vue/01.基础',
+                    }
+                ],
+                activeMatch: "^/web"
+            }
+        ],
+        activeMatch: '^/(web|vue)',
+    },
     {
         text: '系统命令',
         items: [
@@ -92,16 +108,16 @@ export const nav: DefaultTheme.Config['nav'] = [
         ],
         activeMatch: '^/(linux|windows)',
     },
-  {
-    text: 'workflow',
-    items: [
-      {
-        text: 'git',
-        link: '/workflow/01.git/01.git基本操作',
-        activeMatch: '^/workflow/01.git',
-      },
-    ],
-    activeMatch: '^/workflow',
-  },
-  { text: '实用网站', link: '/nav', activeMatch: '^/nav' },
+    {
+        text: 'workflow',
+        items: [
+            {
+                text: 'git',
+                link: '/workflow/01.git/01.git基本操作',
+                activeMatch: '^/workflow/01.git',
+            },
+        ],
+        activeMatch: '^/workflow',
+    },
+    {text: '实用网站', link: '/nav', activeMatch: '^/nav'},
 ]
